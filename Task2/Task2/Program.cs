790db6a45f5fe7fe615b024c00dea666c840b59c
luizasabyr <luiza.sabyr@gmail.com>
2019-02-01 11:52:17 +0600

    ...

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    class Program
+    class Student // создаем класс Student
     {
-        static void Main(string[] args)
+        
+            string name; // строка name
+            string id; // строка id
+            int year = 1; // переменный integer year равный одному
+
+            public Student(string name, string id) //  конструктор с параметрами name  и id
+            {
+             this.name = name; 
+                                  //доступ к текущему экземпляру класса
+            this.id = id;     
+            }
+            public void new_year() //  конструктор без параметров
+            {
+                while (year <= 4)
+                {
+                    Console.WriteLine(name + " " + id + " " + year);
+                   
+                } //выводим имя,id и год обучения,пока год равен или меньше 4.
+            }
+        }
+
+        class Program
         {
+            static void Main(string[] args)
+            {
+                Student s1 = new Student("Sabyr Luiza", "18BD110542"); // новый студент с данными
+                s1.new_year();  // вызов функции new_year
+            }
         }
     }
-}
