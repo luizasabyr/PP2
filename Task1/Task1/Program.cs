a41bc1d8b731b888e74ef86f0d0d20574936f499
luizasabyr <luiza.sabyr@gmail.com>
2019-02-01 11:09:34 +0600

    Initial commit

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -8,9 +8,32 @@ namespace Task1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+            string[] arr = Console.ReadLine().Split();
+            int sum = 0;
+            for (int i = 0; i < a; i++)
+            {
+                 
+                for (int j = 2; j < (int)i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        sum++;
+                        Console.WriteLine(sum);
 
+
+                    }
+                    Console.WriteLine(arr[i]);
+                }
+            }
         }
     }
 }
