12314fde6ee1ecf53b29cfb81e6f6e67c8fbaa86
luizasabyr <luiza.sabyr@gmail.com>
2019-02-01 13:00:12 +0600

    initial commit

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());//создав новую переменную, считываем ее со строки и превращаем ее в целое число
+            int[] arr = new int[n];//создаем новый массив целых чисел
+            string[] array = Console.ReadLine().Split();//считываем каждый элемент строки, как отдельный член массива
+            for (int i = 0; i < n; i++)//пробегаемся по массиву
+            {
+                arr[i] = int.Parse(array[i]);//превращаем каждый элемент массива со string в integer
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " " + arr[i] + " ");//выводим каждый элемент массива по два раза в одну строку
+            }
+        }
+    }
+}
+     
