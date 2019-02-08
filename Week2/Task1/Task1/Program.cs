5a338b2d7d5c45c8780f9bff73504e539b04c56a
luizasabyr <luiza.sabyr@gmail.com>
2019-02-08 12:49:35 +0600

    init commit

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\user\source\repos\ConsoleApp3\123\ConsoleApp4\ConsoleApp4\bin\Debug\input.txt");
+
+            foreach (string line in lines)
+            {
+                char[] charArray = line.ToCharArray();
+                for (int i = 0; i < 1; i++)
+                {
+                    Array.Reverse(charArray);
+                    bool a = charArray.SequenceEqual(line);
+                    while (a == true)
+                    {
+                        Console.WriteLine(line); 
+                        Console.WriteLine("YES");
+                        break;
+                    }
+                    while (a == false)
+                    {
+                        Console.WriteLine(line); 
+                        Console.WriteLine("NO");
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+}
