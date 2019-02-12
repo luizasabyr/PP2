c89759efb9bbac2f58d76e764c7c6f1c514b8a55
luizasabyr <luiza.sabyr@gmail.com>
2019-02-12 17:18:34 +0600

    init commit

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "POP.txt"; // имя файла который я хочу создать
+            string PathString = @"C:\Users\user\Desktop"; // и где я его хочу создать
+            PathString = Path.Combine(PathString, fileName); // нужно скомбинировать их пути
+            FileStream fs = File.Create(PathString); // и создаем файц
+            fs.Close(); // нужно чтобы прекратил работу
+
+            string PathString2 = @"C:\Users\user\Desktop\C#"; // путь куда я должен я скопировать
+
+            string destFile = Path.Combine(PathString2, fileName); // скомбинируем путь
+            File.Copy(PathString, destFile, true); // копируем туда
+
+            File.Delete(PathString); // и удаляем оригинал
+        }
+    }
+}
