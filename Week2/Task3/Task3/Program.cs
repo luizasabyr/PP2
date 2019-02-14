ca7155fb136179b58cdb27e3b06186e370d4c14d
luizasabyr <luiza.sabyr@gmail.com>
2019-02-14 15:09:07 +0600

    .

--- Week2/Task3/Task3/Program.cs
+++ Week2/Task3/Task3/Program.cs
@@ -9,35 +9,37 @@ namespace Task3
 {
     class Program
     {
-        static void PrintSpaces(int level)
+        static void PrintSpaces(int level) //создаем фнукцию PrintSpaces с переменной level типа integer
         {
-            for (int i = 0; i < level; i++)
-                Console.Write("  ");
+            for (int i = 0; i < level; i++)//пробегаемся от нуля до level
+                Console.Write("  "); //выводим пробелы перед новыми папками
         }
 
-        static void Ex4(DirectoryInfo directory, int level)
+        static void Ex4(DirectoryInfo directory, int level)// создаем рекрусивную фунцию, которая выводит названия и все содержимое в папке
         {
-            FileInfo[] files = directory.GetFiles();
-            DirectoryInfo[] directories = directory.GetDirectories();
+            FileInfo[] files = directory.GetFiles();//берет файлы из заданной директории
+            DirectoryInfo[] directories = directory.GetDirectories();//берет папки из заданной директории
 
-            foreach (FileInfo file in files)
+            foreach (FileInfo file in files)// пробегаемся по файлам 
             {
-                PrintSpaces(level);
-                Console.WriteLine(file.Name);
+                PrintSpaces(level); // вызов функции PrintSpaces
+                Console.WriteLine(file.Name); //выводим имя файла
             }
 
-            foreach (DirectoryInfo d in directories)
+            foreach (DirectoryInfo d in directories)// пробегаемся по папкам 
             {
-                PrintSpaces(level);
-                Console.WriteLine(d.Name);
-                Ex4(d, level + 1);
+                PrintSpaces(level);// вызов функции PrintSpaces
+                Console.WriteLine(d.Name);//выводим имя папки
+                Ex4(d, level + 1);//вызов функции Ex4 
+                //добавляем к левелу 1 , чтобы при каждой открытии папки он выводил их на новый уровень, то есть на след строку
             }
         }
 
         static void Main(string[] args)
         {
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\user\source\repos\PP2");
-            Ex4(d, 0);
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\user\source\repos\PP2");//указываем путь
+            Ex4(d, 0);//вызов функции Ex4 
+            //начинаем отступ с нулевого уровня
 
         }
     }
