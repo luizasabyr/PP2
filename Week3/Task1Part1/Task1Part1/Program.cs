47ef5e376fbe9ec0e9354606a823d125fc2af079
luizasabyr <luiza.sabyr@gmail.com>
2019-02-15 15:14:28 +0600

    .

--- Week3/Task1Part1/Task1Part1/Program.cs
+++ Week3/Task1Part1/Task1Part1/Program.cs
@@ -11,63 +11,63 @@ namespace Task1Part1
     {
         static void Main(string[] args)
         {
-            far FarManager = new far();
-            FarManager.Start(@"C:\Users\user\source\repos\PP2");
+            far FarManager = new far(); //создаем FarManager
+            FarManager.Start(@"C:\Users\user\source\repos\PP2");//задаем путь с адресом
         }
     }
-    class far
+    class far //создаем класс
     {
         public int cursor;
-        public int size;
+        public int size;    // создаем переменные курсор,size,ok 
         public bool ok;
         public far()
         {
-            cursor = 0;
+            cursor = 0; //вначале курсор должен стоять на верху поэтому cursor=0
             ok = true;
         }
-        public void Up()
+        public void Up() //создаем метод для того чтобы передвигать курсор вверх
         {
-            cursor--;
-            if (cursor < 0)
-                cursor = size - 1;
+            cursor--; //если курсор -- то он поднимется вверх
+            if (cursor < 0)//если курсор меньше нуля то при нажатии кнопку вверх курсор должен попасть в самый низ поэтому size-1
+                cursor = size - 1; 
         }
-        public void Down()
+        public void Down()//метод для передвигании вниз
         {
-            cursor++;
-            if (cursor == size)
-                cursor = 0;
+            cursor++; //если курсор ++ то он спускается вниз
+            if (cursor == size) //если курсор равен всему размеру тоесть количеству всех файлов и папок то при нажатии кнопку вниз курсор должен оказатся в самом верху
+                cursor = 0;//поэтому курсор =0
         }
-        public void Color(FileSystemInfo file, int index)
+        public void Color(FileSystemInfo file, int index) //создаем функцию для цветов
         {
-            if (index == cursor)
-                Console.BackgroundColor = ConsoleColor.Red;
+            if (index == cursor) //если курсор равен индексу то задний фон должен быть серого цвета
+                Console.BackgroundColor = ConsoleColor.Gray;
             else if (file.GetType() == typeof(DirectoryInfo))
             {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White; //папки белого цвета
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Yellow;//файлы желтого
                 Console.BackgroundColor = ConsoleColor.Black;
             }
         }
-        public void Show(string path)
+        public void Show(string path) //создаем функцию которая показывает нам файлы
         {
             DirectoryInfo directory = new DirectoryInfo(path);
-            FileSystemInfo[] files = directory.GetFileSystemInfos();
-            size = files.Length;
+            FileSystemInfo[] files = directory.GetFileSystemInfos();//показываем все файлы и подкаталоги в том или ином каталоге
+            size = files.Length; 
             int index = 0;
-            foreach (FileSystemInfo fs in files)
+            foreach (FileSystemInfo fs in files)// пробегаемся по файлам
             {
-                Color(fs, index);
+                Color(fs, index); //вызов функции Color
                 Console.WriteLine(fs.Name);
                 index++;
             }
         }
-        public void Start(string path)
+        public void Start(string path) //функция старт для клавиш
         {
-            ConsoleKeyInfo key = Console.ReadKey();
+            ConsoleKeyInfo key = Console.ReadKey();// при нажатии кнопку E вся работа останавливается
             FileSystemInfo fs = null;
             while (key.Key != ConsoleKey.E)
             {
@@ -75,7 +75,7 @@ namespace Task1Part1
                 Console.Clear();
                 Show(path);
                 key = Console.ReadKey();
-                if (key.Key == ConsoleKey.UpArrow)
+                if (key.Key == ConsoleKey.UpArrow) //вызов функции передвигания
                     Up();
                 else if (key.Key == ConsoleKey.DownArrow)
                     Down();
